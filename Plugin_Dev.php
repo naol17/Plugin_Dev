836c91779c52594a4227dfdadb9ec377869c6411
Naol Dame <naododa5566@gmail.com>
2022-01-09 12:42:21 +0300

    Adding plugin headers

---
+++ Plugin_Dev.php
@@ -0,0 +1,14 @@
+/**
+ * Plugin Name:       MyPg
+ * Plugin URI:        https://socialmedia.com
+ * Description:       This is a social media adding plugin to wordpress website helps to add and delete your social media icons .
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Naol Dame
+ * Author URI:        https://authnaoldame.wordpress.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       Plugin_Dev
+ * Domain Path:       /languages
+ */
