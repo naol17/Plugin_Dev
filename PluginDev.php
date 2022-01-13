1ee6c307ff64961574e48f3e897624cdd6a4e152
Naol Dame <naododa5566@gmail.com>
2022-01-13 11:25:48 +0300

    redefining the header and adding additional function

---
+++ PluginDev.php
@@ -0,0 +1,27 @@
+<?php
+/* Plugin Name:       MyPg
+ * Plugin URI:        https://socialmedia.com
+ * Description:       This is a social media adding plugin to wordpress website helps to add and delete your social media icons .
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Naol Dame
+ * Author URI:        https://authnaoldame.wordpress.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       Plugin_Dev
+ * Domain Path:       /languages
+ */
+if(! defined('ABSPATH')){
+    exit;
+}
+
+//  <!-- to get list of social profile that are being used  -->
+
+function mypg_socialprofiles(){
+    //  <!-- return the social profile --> 
+    return apply_filters(
+        'mypg_profiles',
+        array()
+    );
+}
--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <?php
-// This is nonsense. 
+// This is nonsense.
